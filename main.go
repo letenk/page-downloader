24e71b22d7d89ba000ef478ccd63aaa32536a799
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-12-27 14:25:30 +0700

    features: use channel for take response goroutine

--- main.go
+++ main.go
@@ -3,38 +3,47 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"runtime"
 	"strings"
 	"sync"
 )
 
-func checkAndSaveBody(url string, wg *sync.WaitGroup) {
+func checkAndSaveBody(url string, wg *sync.WaitGroup, chn chan string) {
 	resp, err := http.Get(url)
+
 	if err != nil {
-		fmt.Println(err)
-		fmt.Printf("%s is DOWN!\n", url)
+		strError := fmt.Sprintf("%s is Down! \n", url)
+		strError += fmt.Sprintf("Error: %v", err)
+
+		chn <- strError
 	} else {
 		defer resp.Body.Close()
-		fmt.Printf("%s -> Status Code: %d \n", url, resp.StatusCode)
+
+		strResponse := fmt.Sprintf("%s -> Status Code: %d \n", url, resp.StatusCode)
 
 		if resp.StatusCode == 200 {
 			bodyBytes, err := ioutil.ReadAll(resp.Body)
 			if err != nil {
-				log.Fatalf("Read Body err: %v", err)
+				strError := fmt.Sprintf("Read Body err: %v", err)
+				chn <- strError
 			}
 
 			fileName := strings.Split(url, "//")[1] // this will take domain only except `http:`
 			fileName += ".txt"
 
-			fmt.Printf("Writing response body to %s\n", fileName)
+			strResponse += fmt.Sprintf("Writing response body to %s\n", fileName)
 
 			err = ioutil.WriteFile(fileName, bodyBytes, 0664)
 			if err != nil {
-				log.Fatalf("Write file err: %v", err)
+				strError := fmt.Sprintf("Write file err: %v", err)
+				chn <- strError
 			}
 		}
+
+		strResponse += fmt.Sprintf("%s is UP \n", url)
+
+		chn <- strResponse
 	}
 
 	wg.Done()
@@ -43,16 +52,24 @@ func checkAndSaveBody(url string, wg *sync.WaitGroup) {
 func main() {
 	urls := []string{"https://golang1.org", "https://www.google.com", "https://www.medium.com"}
 
+	// Create wait group
 	var wg sync.WaitGroup
+
+	// Create channel
+	chn := make(chan string)
 
 	wg.Add(len(urls))
 
 	for _, url := range urls {
-		go checkAndSaveBody(url, &wg)
-		fmt.Println(strings.Repeat("#", 20))
+		go checkAndSaveBody(url, &wg, chn)
 	}
 
 	fmt.Println("No. of Goroutines:", runtime.NumGoroutine())
+
+	// Receive message from channel
+	for i := 0; i < len(urls); i++ {
+		fmt.Println(<-chn)
+	}
 
 	wg.Wait()
 }
